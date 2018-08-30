acf6b092e9deba3339aa3c819a97a54d598dcbcc
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2018-08-30 14:14:21 +0300

    init

---
+++ capsula-ci-cd/generators/travis.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var path = require('path');
+var isValid = require('is-valid-app');
+
+module.exports = function(app) {
+  if (!isValid(app, 'generate-travis')) return;
+  
+  app.task('travis', { silent: true }, function(cb) {
+    return app.src('_travis.yml', {cwd: path.resolve(__dirname, 'templates')})
+      .pipe(app.dest(function(file) {
+        file.basename = '.travis.yml';
+        return app.cwd;
+      }));
+  });
+  
+  app.task('default', ['travis']);
+};
