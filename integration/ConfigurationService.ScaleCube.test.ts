0800f1f1df72e010584de02995f494e837bd5c13
Taras Shedenko <taras.shedenko@jivygroup.com>
2018-10-24 18:58:57 +0300

    Beginning of the ScaleCube integration test

--- integration/ConfigurationService.ScaleCube.test.ts
+++ integration/ConfigurationService.ScaleCube.test.ts
@@ -1,20 +1,76 @@
-import { AxiosDispatcher } from 'capsulajs-transport-providers';
+(global as any).WebSocket = require('ws');
+
+// import { AxiosDispatcher } from 'capsulajs-transport-providers';
+import { Dispatcher, WebSocketDispatcher } from 'capsulajs-transport-providers';
 import { OrganizationService } from 'account-service';
 
-import { wsUrl } from './constants';
+jest.setTimeout(30000);
+
+import {
+  wsUrl,
+  testOrgName,
+  testOrgEmail,
+  apiKeyName,
+  configRepo,
+} from './constants';
 
 import { getAuth0Token } from './getAuth0Token';
-
-// declare var OrganizationService: any;
 
 describe('Transport providers are available', () => {
 
   it('Creates an instance of the AxiosDispatcher', async () => {
-    const auth0 = await getAuth0Token();
-    console.log('AUTH):\n', auth0);
-    expect(1).toBeTruthy();
-    // const organizationService = new OrganizationService(new AxiosDispatcher(wsUrl));
-    // console.log('ORGANIZATION SERVICE:\n', organizationService);
-    // expect(organizationService.constructor).toBe(OrganizationService);
+    expect.assertions(4);
+
+    let dispatcher: Dispatcher;
+    try {
+
+      const { data: { access_token: auth0Token } } = await getAuth0Token();
+      expect(auth0Token).toBeTruthy();
+      console.log('Auth0 Token:\n', auth0Token);
+  
+      const dispatcher = new WebSocketDispatcher(wsUrl);
+      const orgService = new OrganizationService(dispatcher);
+  
+      let response  = await orgService.createOrganization({
+        token: {
+          issuer: 'Auth0',
+          token: auth0Token,
+        },
+        name: testOrgName,
+        email: testOrgEmail,
+      });
+      const orgId = response.id;
+      expect(orgId).toBeTruthy();
+      console.log('Organization created:\n', orgId);
+
+      response = await orgService.addOrganizationApiKey({
+        token: {
+          issuer: 'Auth0',
+          token: auth0Token,
+        },
+        organizationId: orgId,
+        apiKeyName,
+        claims: { role: 'Owner' },
+      });
+      const apiKey = response.apiKeys[0].key;
+      expect(apiKey).toBeTruthy();
+      console.log('Api Key created:\n', apiKey);
+
+      response = await orgService.deleteOrganization({
+        token: {
+          issuer: 'Auth0',
+          token: auth0Token,
+        },
+        organizationId: orgId,
+      });
+      expect(response.deleted).toBe(true);
+      console.log('Organization deleted: ', response.deleted);
+
+      dispatcher.finalize && dispatcher.finalize();
+    }
+    catch (err) {
+      console.log('ERROR DURING EXECUTION:\n', err);
+      throw err;
+    }
   });
 });
--- src/provider/ScaleCube/ConfigurationServiceScaleCube.ts
+++ src/provider/ScaleCube/ConfigurationServiceScaleCube.ts
@@ -1,65 +1,42 @@
-import axios from 'axios';
+import { Dispatcher } from 'capsulajs-transport-providers';
 
-import { ConfigurationService } from 'src/api';
+import { ConfigurationServiceBase } from '../../api';
 
-export class ConfigurationServiceScaleCube implements ConfigurationService {
-  constructor(
-    private baseUrl: string,
-    private organizationId: string,
-    private apiKey: string,
-    private collectionName: string,
-  ) {
+export class ConfigurationServiceScaleCube<T=any> extends ConfigurationServiceBase<T> {
+  private dispatcher: Dispatcher;
+
+  constructor(configName: string, dispatcher: Dispatcher) {
+    super(configName);
+    this.dispatcher = dispatcher;
   };
-
-  private doHttpRequest(url: string, data?: any) {
-    // TO BE DONE
-    return axios.post(
-      this.baseUrl + url,
-      {
-        token: {
-          origin: this.organizationId,
-          token: this.apiKey,
-        },
-        collection: this.collectionName,
-        ...data
-      },
-    ).then(response => {
-      const {status, statusText, data } = response;
-      if (status === 200) {
-        return response.data;
-      } else {
-        throw new Error(`Error during HTTP request: ${status} ${statusText}`);
-      }
-    });
-  }
 
   deleteAll(): Promise<void> {
     // TO BE DONE
-    return this.doHttpRequest('/configuration/delete', { key: null });
+    return this.dispatcher.dispatch('/configuration/delete', { key: null } );
   };
 
   deleteKey(key: string): Promise<void> {
     // TO BE DONE
-    return this.doHttpRequest('/configuration/delete', { key });
+    return this.dispatcher.dispatch('/configuration/delete', { key });
   }
 
   get<T>(key: string): Promise<T> {
     // TO BE DONE
-    return this.doHttpRequest('/configuration/fetch', { key });
+    return this.dispatcher.dispatch('/configuration/fetch', { key });
   }
 
   keys(): Promise<Array<string>> {
     // TO BE DONE
-    return this.doHttpRequest('/configuration/fetch', { });
+    return this.dispatcher.dispatch('/configuration/fetch', { });
   }
 
   set<T>(key: string, value: T): Promise<void> {
     // TO BE DONE
-    return this.doHttpRequest('/configuration/save', { key, value });
+    return this.dispatcher.dispatch('/configuration/save', { key, value });
   }
 
   values<T>(): Promise<Array<T>> {
     // TO BE DONE
-    return this.doHttpRequest('/configuration/fetch', { });
+    return this.dispatcher.dispatch('/configuration/fetch', { });
   }
 }
