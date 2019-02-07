95493798ad37b26bf705f7dd866eae5df96b8a32
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-02-07 15:24:23 +0200

    wip

--- src/provider/FileProvider/ConfigurationServiceFile.ts
+++ src/provider/FileProvider/ConfigurationServiceFile.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import {
   ConfigurationService,
   CreateRepositoryRequest,
@@ -14,77 +15,35 @@ import {
 import { messages, repositoryRequestValidator, repositoryKeyRequestValidator } from '../../utils';
 
 export class ConfigurationServiceFile<T=any> implements ConfigurationService<T> {
-  constructor(private token: string) {
-    if (!this.token) {
-      throw new Error(messages.tokenNotProvided);
-    }
-  }
-  
-  private getRepository(repository: string) {
-    const rawString = localStorage.getItem(`${this.token}.${repository}`);
-    
-    if (!rawString) {
-      return Promise.reject(
-        new Error(`Configuration repository ${repository} not found`)
-      );
+  constructor(private filename: string) {
+    if (!this.filename) {
+      throw new Error(messages.filenameNotProvided);
     }
     
-    try {
-      return Promise.resolve(JSON.parse(rawString));
-    } catch (error) {
-      return Promise.reject(error);
-    }
+    fs.open(filename,'r',function(err) {
+      if (err) {
+        throw new Error(messages.fileNotExist);
+      }
+    });
   }
   
   createRepository(request: CreateRepositoryRequest): Promise<CreateRepositoryResponse> {
-    if (repositoryRequestValidator(request)) {
-      return Promise.reject(new Error(messages.repositoryNotProvided));
-    }
-    
-    return new Promise((resolve, reject) => {
-      this.getRepository(request.repository).then(() => {
-        reject(new Error(messages.repositoryAlreadyExists));
-      }).catch(() => {
-        localStorage.setItem(`${this.token}.${request.repository}`, JSON.stringify({}));
-        return resolve({ repository: request.repository });
-      });
-    });
+    return Promise.reject(new Error(messages.notImplemented));
   }
   
   delete(request: DeleteRequest): Promise<DeleteResponse> {
-    if (repositoryRequestValidator(request)) {
-      return Promise.reject(new Error(messages.repositoryNotProvided));
-    }
-    
-    return new Promise((resolve, reject) => {
-      this.getRepository(request.repository).then((repository) => {
-        if (request.key) {
-          if (!repository.hasOwnProperty(request.key)) {
-            reject(new Error(`Configuration repository key ${request.key} not found`));
-          }
-          
-          const { [request.key]: value, ...rest } = repository;
-          localStorage.setItem(`${this.token}.${request.repository}`, JSON.stringify(rest));
-        } else {
-          localStorage.removeItem(`${this.token}.${request.repository}`);
-        }
-        
-        resolve({});
-      }).catch(reject);
-    });
+    return Promise.reject(new Error(messages.notImplemented));
   }
   
-  entries(request: EntriesRequest): Promise<EntriesResponse<T>> {
+  entries(request: EntriesRequest): Promise<any> { // EntriesResponse
     if (repositoryRequestValidator(request)) {
       return Promise.reject(new Error(messages.repositoryNotProvided));
     }
-    
-    return this.getRepository(request.repository).then(repository => ({
-      entries: Object.keys(repository).map(key => ({ key, value: repository[key] }))
-    }));
+  
+    return Promise.resolve({});
   };
   
-  fetch(request: FetchRequest): Promise<FetchResponse> {
+  fetch(request: FetchRequest): Promise<any> { // FetchResponse
     if (repositoryRequestValidator(request)) {
       return Promise.reject(new Error(messages.repositoryNotProvided));
     }
@@ -92,34 +51,11 @@ export class ConfigurationServiceFile<T=any> implements ConfigurationService<T> 
     if (repositoryKeyRequestValidator(request)) {
       return Promise.reject(new Error(messages.repositoryKeyNotProvided));
     }
-    
-    return new Promise((resolve, reject) => {
-      this.getRepository(request.repository).then(repository =>
-        Object.keys(repository).indexOf(request.key) >= 0
-          ? resolve({ key: request.key, value: repository[request.key] })
-          : reject(new Error(`Configuration repository key ${request.key} not found`))
-      ).catch(reject);
-    });
+  
+    return Promise.resolve({});
   }
   
   save(request: SaveRequest): Promise<SaveResponse> {
-    if (repositoryRequestValidator(request)) {
-      return Promise.reject(new Error(messages.repositoryNotProvided));
-    }
-    
-    if (repositoryKeyRequestValidator(request)) {
-      return Promise.reject(new Error(messages.repositoryKeyNotProvided));
-    }
-    
-    return new Promise((resolve, reject) => {
-      this.getRepository(request.repository).then((repository) => {
-        localStorage.setItem(`${this.token}.${request.repository}`, JSON.stringify({
-          ...repository,
-          [request.key]: request.value
-        }));
-        
-        resolve({});
-      }).catch(reject);
-    });
+    return Promise.reject(new Error(messages.notImplemented));
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,9 @@
 export const repositoryRequestValidator = (request: any) => request === undefined || request.repository === undefined;
 export const repositoryKeyRequestValidator = (request: any) => request.key === undefined;
 export const messages = {
+  notImplemented: 'Configuration repository method not implemented yet',
+  filenameNotProvided: 'Configuration repository filename not provided',
+  fileNotExist: 'Configuration repository file not exist',
   tokenNotProvided: 'Configuration repository token not provided',
   repositoryNotProvided: 'Configuration repository not provided',
   repositoryKeyNotProvided: 'Configuration repository key not provided',
