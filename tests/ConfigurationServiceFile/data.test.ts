aadf207c01d2c1a5d1c78d414df0d88919f5454a
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-02-08 10:19:41 +0200

    wip

--- tests/ConfigurationServiceFile/data.test.ts
+++ tests/ConfigurationServiceFile/data.test.ts
@@ -1,65 +1,23 @@
 import { ConfigurationService } from 'api/ConfigurationService';
 import { ConfigurationServiceFile } from 'provider/FileProvider';
 
-const token = 'token';
+const filename = 'test.conf.json';
 const repository = 'Adele';
 const key = 'Hello';
 const value = 'It\'s me';
-const configService = new ConfigurationServiceFile(token);
+const configService = new ConfigurationServiceFile(filename);
 
 describe('Test suite for the ConfigurationServiceFile', () => {
-  beforeEach(localStorage.clear);
-  
-  it('createRepository() should create configuration repository', async () => {
+  it('entries() should return all values and keys', async () => {
     expect.assertions(1);
-    expect(1 + 1).toEqual(2);
-    
-    // expect(await configService.createRepository({ repository })).toEqual({ repository });
-    // expect(await configService.entries({ repository })).toEqual({ entries: [] });
+    expect(await configService.entries({ repository })).toEqual({
+      entries: [{ key, value }, { key: 'Surprise', value: 'I have a balloon' }]
+    });
   });
-  
-  // it('delete() should return empty object and delete configuration repository key', async () => {
-  //   expect.assertions(5);
-  //   expect(await configService.createRepository({ repository })).toEqual({ repository });
-  //   expect(await configService.save({ repository, key, value })).toEqual({});
-  //   expect(await configService.entries({ repository })).toEqual({ entries: [{ key, value }] });
-  //   expect(await configService.delete({ repository, key })).toEqual({});
-  //   expect(await configService.entries({ repository })).toEqual({ entries: [] });
-  // });
-  //
-  // it('entries() should return all values and keys', async () => {
-  //   expect.assertions(4);
-  //   expect(await configService.createRepository({ repository })).toEqual({ repository });
-  //   expect(await configService.save({ repository, key, value })).toEqual({});
-  //   expect(await configService.save({
-  //     repository, key: 'Surprise', value: 'I have a balloon'
-  //   })).toEqual({});
-  //   expect(await configService.entries({ repository })).toEqual({
-  //     entries: [{ key, value }, { key: 'Surprise', value: 'I have a balloon' }]
-  //   });
-  // });
-  //
-  // it('fetch() should return value by key', async () => {
-  //   expect.assertions(3);
-  //   expect(await configService.createRepository({ repository })).toEqual({ repository });
-  //   expect(await configService.save({ repository, key, value })).toEqual({});
-  //   expect(await configService.fetch({ repository, key })).toEqual({ key, value });
-  // });
-  //
-  // it('save() should persist value by key', async () => {
-  //   expect.assertions(3);
-  //   expect(await configService.createRepository({ repository })).toEqual({ repository });
-  //   expect(await configService.save({ repository, key, value })).toEqual({});
-  //   expect(await configService.entries({ repository })).toEqual({ entries: [{ key, value }] });
-  // });
-  //
-  // it('save() should persist new value by the same key', async () => {
-  //   expect.assertions(5);
-  //   expect(await configService.createRepository({ repository })).toEqual({ repository });
-  //   expect(await configService.save({ repository, key, value })).toEqual({});
-  //   expect(await configService.entries({ repository })).toEqual({ entries: [{ key, value }] });
-  //   expect(await configService.save({ repository, key, value: 'Goodbye' })).toEqual({});
-  //   expect(await configService.entries({ repository })).toEqual({ entries: [{ key, value: 'Goodbye' }] });
-  // });
+
+  it('fetch() should return value by key', async () => {
+    expect.assertions(1);
+    expect(await configService.fetch({ repository, key })).toEqual({ key, value });
+  });
   
 });
--- tests/ConfigurationServiceFile/errors.test.ts
+++ tests/ConfigurationServiceFile/errors.test.ts
@@ -5,7 +5,6 @@ import { messages } from '../../src/utils';
 const filename = 'test.conf.json';
 const repository = 'Adele';
 const key = 'Hello';
-const value = 'It\'s me';
 const configService = new ConfigurationServiceFile(filename);
 
 describe('Test suite for the ConfigurationServiceFile', () => {
@@ -14,7 +13,7 @@ describe('Test suite for the ConfigurationServiceFile', () => {
     expect.assertions(1);
     expect(() => new ConfigurationServiceFile()).toThrow(new Error(messages.filenameNotProvided));
   });
-  
+
   it('New instance of service should throw \'fileOrDirectoryNotExist\' error', async () => {
     expect.assertions(1);
     expect(() => new ConfigurationServiceFile('test.conf.not.exist.json'))
@@ -36,51 +35,33 @@ describe('Test suite for the ConfigurationServiceFile', () => {
     });
   });
 
-  // it('Call fetch(), entries() with unexisting repository should' +
-  //   'return \'Configuration repository is not found\' error', async () => {
-  //   expect.assertions(2);
-  //   ['entries', 'fetch'].forEach((method) => {
-  //     configService[method]({ repository, key }).catch(
-  //       error => expect(error).toEqual(new Error(`Configuration repository ${repository} not found`))
-  //     );
-  //   });
-  // });
-  
-  
-  
-  
-  
-  
-  // it('Call fetch() and save() without providing key should return \'repositoryKeyNotProvided\' error', async () => {
-  //   expect.assertions(2);
-  //   await configService.createRepository({ repository });
-  //   ['fetch', 'save'].forEach((method) => {
-  //     configService[method]({ repository }).catch(
-  //       error => expect(error).toEqual(new Error(messages.repositoryKeyNotProvided))
-  //     );
-  //   });
-  // });
-  //
-  // it('Call delete(), fetch()  with unexisting key should' +
-  //   'return \'Configuration repository key not found\' error', async () => {
-  //   expect.assertions(2);
-  //   await configService.createRepository({ repository });
-  //   ['delete', 'fetch'].forEach((method) => {
-  //     configService[method]({ repository, key }).catch(
-  //       error => expect(error).toEqual(new Error(`Configuration repository key ${key} not found`))
-  //     );
-  //   });
-  // });
-  //
-  // it('delete() without key should delete configuration repository', async () => {
-  //   expect.assertions(5);
-  //   expect(await configService.createRepository({ repository })).toEqual({ repository });
-  //   expect(await configService.save({ repository, key, value })).toEqual({});
-  //   expect(await configService.entries({ repository })).toEqual({ entries: [{ key, value }] });
-  //   expect(await configService.delete({ repository })).toEqual({});
-  //   configService.entries({ repository }).catch(
-  //     error => expect(error).toEqual(new Error(`Configuration repository ${repository} not found`))
-  //   );
-  // });
+  it('Call fetch(), entries() with unexisting repository should' +
+    'return \'Configuration repository is not found\' error', async () => {
+    expect.assertions(2);
+    ['entries', 'fetch'].forEach((method) => {
+      configService[method]({ repository: 'Not Adele', key }).catch(
+        error => expect(error).toEqual(new Error(`Configuration repository ${repository} not found`))
+      );
+    });
+  });
+
+  it('Call fetch() without providing key should return \'repositoryKeyNotProvided\' error', async () => {
+    expect.assertions(1);
+    ['fetch'].forEach((method) => {
+      configService[method]({ repository }).catch(
+        error => expect(error).toEqual(new Error(messages.repositoryKeyNotProvided))
+      );
+    });
+  });
+
+  it('Call fetch()  with unexisting key should' +
+    'return \'Configuration repository key not found\' error', async () => {
+    expect.assertions(1);
+    ['fetch'].forEach((method) => {
+      configService[method]({ repository, key: 'Goodbye' }).catch(
+        error => expect(error).toEqual(new Error(`Configuration repository key Goodbye not found`))
+      );
+    });
+  });
   
 });
