55d641a6b3b0a9fb031ac1f0269f80bad3c7a2aa
Michael Arbib <michael.arbib@om2.com>
2019-02-20 16:41:36 +0200

    Fixed version and gitignore (#13)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,5 @@
 export { ConfigurationService } from './api';
 export { ConfigurationServiceHardcoreRemote } from './provider/HardcoreRemote';
 export { ConfigurationServiceLocalStorage } from './provider/LocalStorage';
+export { ConfigurationServiceFile } from './provider/FileProvider';
+export { messages } from './utils';
