208fc413717de9a5261ba3758ad665289c8832cd
dmitriyslaym <dmitriyslaym@gmail.com>
2019-05-31 09:55:03 +0300

    Removed code for manual debugging

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,3 @@ export { ConfigurationServiceFile } from './provider/FileProvider';
 export { ConfigurationServiceHttp } from './provider/HttpProvider';
 export { ConfigurationServiceHttpFile } from './provider/HttpFile';
 export { messages } from './utils';
-
-// const configurationServiceHttpFile = new ConfigurationServiceHttpFile('localhost:3000');
-//
-// configurationServiceHttpFile.entries({ repository: 'workspace7' }).then((response) => {
-//   console.log('entries response', response)
-// }).catch((error) => {
-//   console.log('error', error);
-// })
-//
-// export { ConfigurationServiceHttpFile };
-
