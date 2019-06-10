f00ed5dcac165e8d9f878f690ec1da08ba030e06
dmitriyslaym <dmitriyslaym@gmail.com>
2019-06-10 17:23:12 +0300

    Improved description

--- src/api/GetProvider.ts
+++ src/api/GetProvider.ts
@@ -1,7 +1,7 @@
 import { ConfigurationService, GetProviderRequest } from '.';
 
 /**
- * Returns the class of a ConfigurationService based on the configurationType
+ * Returns the class of a ConfigurationProvider based on a specific configuration provider name
  * @param getProviderRequest
  */
 export type GetProvider = (getProviderRequest: GetProviderRequest) => new (configurationServiceConfig: any) => ConfigurationService;
