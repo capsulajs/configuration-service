e1af8aa7dbd0e186d2426cad8b57bb69d7cd937c
dmitriyslaym <dmitriyslaym@gmail.com>
2019-06-10 19:48:53 +0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export { ConfigurationServiceLocalStorage } from './provider/LocalStorage';
 export { ConfigurationServiceFile } from './provider/FileProvider';
 export { ConfigurationServiceHttp } from './provider/HttpProvider';
 export { ConfigurationServiceHttpFile } from './provider/HttpFile';
-export { messages } from './utils';
+export { messages, getProvider } from './utils';
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,9 @@
 import { Repository } from './api';
+import { ConfigurationServiceHardcoreRemote } from './provider/HardcoreRemote';
+import { ConfigurationServiceLocalStorage } from './provider/LocalStorage';
+import { ConfigurationServiceFile } from './provider/FileProvider';
+import { ConfigurationServiceHttp } from './provider/HttpProvider';
+import { ConfigurationServiceHttpFile } from './provider/HttpFile';
 
 export const repositoryRequestValidator = (request: any) => request === undefined || request.repository === undefined;
 
@@ -12,6 +17,14 @@ export const messages = {
   repositoryAlreadyExists: 'Configuration repository already exists'
 };
 
+export const configurationTypes = {
+  localFile: 'localFile',
+  hardcoreServer: 'hardcoreServer',
+  httpFile: 'httpFile',
+  httpServer: 'httpServer',
+  localStorage: 'localStorage',
+};
+
 export const fetchFile = (token: string, fileName: string) => fetch(`http://${token}/${fileName}.json`)
   .then((response) => response.json());
 
@@ -21,3 +34,23 @@ export const formatRepositoryToEntries = (repository: Repository) => Object.keys
 export const getRepositoryNotFoundErrorMessage = (repository: string) => `Configuration repository ${repository} not found`;
 
 export const getRepositoryKeyNotFoundErrorMessage = (key: string) => `Configuration repository key ${key} not found`;
+
+export const getProvider = (getProviderRequest: any) => {
+  switch (getProviderRequest.configProvider) {
+    case configurationTypes.httpServer: {
+      return ConfigurationServiceHttp;
+    }
+    case configurationTypes.hardcoreServer: {
+      return ConfigurationServiceHardcoreRemote;
+    }
+    case configurationTypes.httpFile: {
+      return ConfigurationServiceHttpFile;
+    }
+    case configurationTypes.localFile: {
+      return ConfigurationServiceFile;
+    }
+    case configurationTypes.localStorage: {
+      return ConfigurationServiceLocalStorage;
+    }
+  }
+};
