0d3036a826f08c6cd13fbd33cac4bfed86af7e9b
dmitriyslaym <dmitriyslaym@gmail.com>
2019-06-12 16:55:13 +0300

    Update package version and added more exports to the root

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import * as API from './api';
+
+export { API };
 export { ConfigurationService } from './api';
 export { ConfigurationServiceScalecube } from './provider/Scalecube';
 export { ConfigurationServiceLocalStorage } from './provider/LocalStorage';
 export { ConfigurationServiceFile } from './provider/FileProvider';
 export { ConfigurationServiceHttp } from './provider/HttpProvider';
 export { ConfigurationServiceHttpFile } from './provider/HttpFile';
-export { messages, getProvider } from './utils';
+export { messages, getProvider, configurationTypes } from './utils';
