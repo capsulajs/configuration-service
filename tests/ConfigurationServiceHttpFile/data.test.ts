b7b54d57c73e62662fbd82bb97917e906c033c6d
Michael <paliukh.mykhailo@gmail.com>
2019-07-18 11:01:31 +0300

    Add cache support for httpFile Provider (#32)
    * http file provider updates
    
    * renamed cache variable to be more sugestive
    
    * renamed cache variable to be more sugestive
    
    * Update src/provider/HttpFile/ConfigurationServiceHttpFile.ts
    
    Co-Authored-By: Michael Arbib <michael.arbib@om2.com>
    
    * code review improvements
    
    * code review improvements
    
    * added tests

--- tests/ConfigurationServiceHttpFile/data.test.ts
+++ tests/ConfigurationServiceHttpFile/data.test.ts
@@ -7,25 +7,43 @@ const key = 'artist';
 const value = 'Oxxy';
 const surprise = 'This is all Matrix';
 const configurationData = {
-  [key]: value,
-  Surprise: surprise
+    [key]: value,
+    Surprise: surprise
 };
 
-const configService = new ConfigurationServiceHttpFile(token);
-
-describe('Test suite for the ConfigurationServiceHttpFile', () => {
-  it('entries() should return all values and keys', async () => {
-    expect.assertions(1);
-    mockFetchFile({ type: 'resolve', content: configurationData });
-    return expect(configService.entries({ repository })).resolves.toEqual({
-      entries: [{ key, value }, { key: 'Surprise', value: surprise }]
+describe('Test suite for the ConfigurationServiceHttpFile(token)', () => {
+    it('entries() should return all values and keys', async () => {
+        expect.assertions(1);
+        mockFetchFile({ type: 'resolve', content: configurationData });
+        const configService = new ConfigurationServiceHttpFile(token);
+        return expect(configService.entries({ repository })).resolves.toEqual({
+            entries: [{ key, value }, { key: 'Surprise', value: surprise }]
+        });
     });
-  });
 
-  it('fetch() should return value by key', async () => {
-    expect.assertions(1);
-    mockFetchFile({ type: 'resolve', content: configurationData });
-    return expect(configService.fetch({ repository, key })).resolves.toEqual({ key, value });
-  });
-
+    it('fetch() should return value by key', async () => {
+        expect.assertions(1);
+        const configService = new ConfigurationServiceHttpFile(token);
+        mockFetchFile({ type: 'resolve', content: configurationData });
+        return expect(configService.fetch({ repository, key })).resolves.toEqual({ key, value });
+    });
 });
+
+describe('Test suite for the ConfigurationServiceHttpFile( {token: token, disableCache: true/false} )', () => {
+    it('entries() should return all values and keys', async () => {
+        expect.assertions(1);
+        mockFetchFile({ type: 'resolve', content: configurationData });
+        const configService = new ConfigurationServiceHttpFile({ token, disableCache: true });
+        return expect(configService.entries({ repository })).resolves.toEqual({
+            entries: [{ key, value }, { key: 'Surprise', value: surprise }]
+        });
+    });
+
+    it('fetch() should return value by key, disableCache = false', async () => {
+        expect.assertions(1);
+        const configService = new ConfigurationServiceHttpFile({ token, disableCache: false });
+        mockFetchFile({ type: 'resolve', content: configurationData });
+        return expect(configService.fetch({ repository, key })).resolves.toEqual({ key, value });
+    });
+});
+
