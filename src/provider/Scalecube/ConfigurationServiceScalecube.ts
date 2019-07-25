734dc86abd4fd18fc169a0244fe54bf8d0c8bae2
IrinaComindaru <irina.comindaru@jivygroup.com>
2019-07-25 17:07:41 +0300

    Adjustments after changes in Remote Configurations Service (#34)
    * added implementation of the changes
    
    * added implementation of the changes
    
    * update feature files
    
    * formatting
    
    * formatting
    
    * Update errors.feature
    
    * Update data.feature
    
    * update feature files
    
    * update feature files

--- src/provider/Scalecube/ConfigurationServiceScalecube.ts
+++ src/provider/Scalecube/ConfigurationServiceScalecube.ts
@@ -1,35 +1,28 @@
-import { Dispatcher } from '@capsulajs/capsulajs-transport-providers';
+import {Dispatcher} from '@capsulajs/capsulajs-transport-providers';
 import {
   ConfigurationService,
   CreateRepositoryRequest,
   CreateRepositoryResponse,
   DeleteRequest,
   DeleteResponse,
+  Entity,
   EntriesRequest,
   EntriesResponse,
-  Entity,
   FetchRequest,
   FetchResponse,
   SaveRequest,
   SaveResponse,
 } from '../../api';
-import { messages, repositoryKeyRequestValidator, repositoryRequestValidator } from '../../utils';
+import {messages, repositoryKeyRequestValidator, repositoryRequestValidator} from '../../utils';
 
 const endpoint = '/configuration';
 
-export class ConfigurationServiceScalecube<T=any> implements ConfigurationService<T> {
+export class ConfigurationServiceScalecube<T = any> implements ConfigurationService<T> {
   constructor(private token: string, private dispatcher: Dispatcher) {
     if (!this.token) {
       throw new Error(messages.tokenNotProvided);
     }
   };
-
-  private prepRequest(request: any): any {
-    return {
-      token: this.token,
-      ...request
-    };
-  }
 
   createRepository(request: CreateRepositoryRequest): Promise<CreateRepositoryResponse> {
     if (repositoryRequestValidator(request)) {
@@ -44,7 +37,11 @@ export class ConfigurationServiceScalecube<T=any> implements ConfigurationServic
       return Promise.reject(new Error(messages.repositoryNotProvided));
     }
 
-    return this.dispatcher.dispatch(`${endpoint}/delete`, this.prepRequest(request));
+    if (repositoryKeyRequestValidator(request)) {
+      return Promise.reject(new Error(messages.repositoryKeyNotProvided));
+    }
+
+    return this.dispatcher.dispatch(`${endpoint}/deleteEntry`, this.prepRequest(request));
   }
 
   entries<T>(request: EntriesRequest): Promise<EntriesResponse<T>> {
@@ -52,7 +49,7 @@ export class ConfigurationServiceScalecube<T=any> implements ConfigurationServic
       return Promise.reject(new Error(messages.repositoryNotProvided));
     }
 
-    return this.dispatcher.dispatch<any, Entity<T>[]>(`${endpoint}/entries`, this.prepRequest(request))
+    return this.dispatcher.dispatch<any, Entity<T>[]>(`${endpoint}/readList`, this.prepRequest(request))
       .then(entries => ({entries}));
   }
 
@@ -65,10 +62,10 @@ export class ConfigurationServiceScalecube<T=any> implements ConfigurationServic
       return Promise.reject(new Error(messages.repositoryKeyNotProvided));
     }
 
-    return this.dispatcher.dispatch(`${endpoint}/fetch`, this.prepRequest(request));
+    return this.dispatcher.dispatch(`${endpoint}/readEntry`, this.prepRequest(request));
   }
 
-  save(request: SaveRequest<T>): Promise<SaveResponse> {
+  async save(request: SaveRequest<T>): Promise<SaveResponse> {
     if (repositoryRequestValidator(request)) {
       return Promise.reject(new Error(messages.repositoryNotProvided));
     }
@@ -77,6 +74,28 @@ export class ConfigurationServiceScalecube<T=any> implements ConfigurationServic
       return Promise.reject(new Error(messages.repositoryKeyNotProvided));
     }
 
-    return this.dispatcher.dispatch(`${endpoint}/save`, this.prepRequest(request));
+    const createResult: Promise<SaveResponse> = this.dispatcher.dispatch(`${endpoint}/createEntry`, this.prepRequest(request));
+    try {
+      await createResult;
+      return createResult;
+    } catch (exception) {
+      if (exception && exception.errorMessage === this.keyAlreadyExistsError(request.repository, request.key)) {
+        return this.dispatcher.dispatch(`${endpoint}/updateEntry`, this.prepRequest(request));
+      }
+      else {
+        return createResult;
+      }
+    }
+  }
+
+  private prepRequest(request: any): any {
+    return {
+      apiKey: this.token,
+      ...request
+    };
+  }
+
+  private keyAlreadyExistsError(repository: string, key: string): string {
+    return `Repository '${repository}' key '${key}' already exists`;
   }
 }
---
+++ tests/ConfigurationServiceScalecube/errors.test.ts
@@ -0,0 +1,82 @@
+import {AxiosDispatcher} from '@capsulajs/capsulajs-transport-providers';
+import {ConfigurationServiceScalecube} from '../../src/provider/Scalecube/ConfigurationServiceScalecube';
+import {messages} from '../../src';
+import {getRepositoryKeyNotFoundErrorMessage, getRepositoryNotFoundErrorMessage} from '../../src/utils';
+import {runTestsRejectedError} from '../utils';
+
+const token = 'token';
+const emptyToken = '';
+const repository = 'Adele';
+const key = 'Hello';
+const dispatcher = new AxiosDispatcher(`${token}`);
+const mockedCallback = jest.fn();
+dispatcher.dispatch = mockedCallback;
+const configService = new ConfigurationServiceScalecube(token, dispatcher);
+
+interface RemoteConfigurationServerError {
+  errorCode: number;
+  errorMessage: string;
+}
+
+afterEach(() => {
+  jest.restoreAllMocks();
+  mockedCallback.mockClear();
+});
+
+describe('Errors tests for the ConfigurationServiceScalecube', () => {
+  it('Create new instance of service without providing a token should throw an error', async () => {
+    expect.assertions(1);
+    return expect(() => new ConfigurationServiceScalecube(emptyToken, dispatcher)).toThrow(new Error(messages.tokenNotProvided));
+  });
+
+  it('Call any method from configurationServiceScalecube without repository in the request - error is thrown', async () => {
+    expect.assertions(5);
+    ['delete', 'createRepository', 'entries', 'fetch', 'save'].forEach((method) => {
+      // @ts-ignore
+      expect(configService[method]({})).rejects.toEqual(new Error(messages.repositoryNotProvided));
+    });
+  });
+
+  it('Call delete(), entries(), fetch() and save() methods from configurationServiceScalecube with not-existent repository should throw error', async () => {
+    expect.assertions(4);
+    const exception: RemoteConfigurationServerError = {
+      errorCode: 500,
+      errorMessage: getRepositoryNotFoundErrorMessage(repository)
+    };
+    ['delete', 'entries', 'fetch', 'save'].forEach((method) => {
+      mockedCallback.mockRejectedValueOnce(exception);
+      // @ts-ignore
+      expect(configService[method]({repository, key})).rejects.toEqual(exception);
+    });
+  });
+
+  it('Call fetch(), save(), delete() without providing key should throw an error', (done) => {
+    runTestsRejectedError(expect, done)(
+      configService,
+      ['fetch', 'save', 'delete'],
+      {repository, undefined},
+      new Error(messages.repositoryKeyNotProvided)
+    );
+  });
+
+  it('Call fetch(), save(), delete() with not-existent key should throw an error', (done) => {
+    const exception: RemoteConfigurationServerError = {
+      errorCode: 500,
+      errorMessage: getRepositoryKeyNotFoundErrorMessage(key)
+    };
+    runTestsRejectedError(expect, done)(
+      configService,
+      ['fetch', 'save', 'delete'],
+      {repository, key},
+      exception,
+      () => mockedCallback.mockRejectedValueOnce(exception)
+    );
+  });
+
+  it('createRepository() providing an existing repository name should return error', async () => {
+    expect.assertions(1);
+    const exception: RemoteConfigurationServerError = {errorCode: 500, errorMessage: messages.repositoryAlreadyExists};
+    mockedCallback.mockRejectedValueOnce(exception);
+    return expect(configService.createRepository({repository})).rejects.toEqual(exception);
+  });
+});
