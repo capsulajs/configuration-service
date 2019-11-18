6dae9f2a1f8feb000f265efd47f0402a1387bede
Vladyslav Kazantsev <3955277+specialistvlad@users.noreply.github.com>
2019-11-18 14:12:25 +0200

    Fix concurency bug when user use save() for localStorage provider simultaneously (#43)
    * Added test for localstorage provider for 'save' method. It shows wrong results under some concurency conditions.
    
    * fix concurency bug
    
    * refactoring places where use localStorage.setItem method
    
    * don't use private unsafe method 'getRepository'from createRepository
    
    * mark async method getRepository as unsafe
    
    * rename getRepositorySync to getRepository
    
    * don't use private UNSAFE_getRepository from delete
    
    * don't use private UNSAFE_getRepository from fetch
    
    * don't use private UNSAFE_getRepository from changeEntry
    
    * remove unused method

--- src/provider/LocalStorage/ConfigurationServiceLocalStorage.ts
+++ src/provider/LocalStorage/ConfigurationServiceLocalStorage.ts
@@ -23,101 +23,92 @@ export class ConfigurationServiceLocalStorage<T=any> implements ConfigurationSer
 
   private getRepository(repository: string) {
     const rawString = localStorage.getItem(`${this.token}.${repository}`);
-
     if (!rawString) {
-      return Promise.reject(
-        new Error(messages.repositoryDoesNotExist(repository))
-      );
+      throw new Error(messages.repositoryDoesNotExist(repository));
+    }
+
+    return JSON.parse(rawString);
+  }
+
+  private setRepository = (repository: string, value: object) => {
+    localStorage.setItem(`${this.token}.${repository}`, JSON.stringify(value));
+  }
+
+  async createRepository(request: CreateRepositoryRequest): Promise<CreateRepositoryResponse> {
+    if (repositoryRequestValidator(request)) {
+      throw new Error(messages.repositoryNotProvided);
     }
 
     try {
-      return Promise.resolve(JSON.parse(rawString));
-    } catch (error) {
-      return Promise.reject(error);
+      this.getRepository(request.repository);
+    } catch {
+      this.setRepository(request.repository, {});
+      return { repository: request.repository };
     }
+    throw new Error(messages.repositoryAlreadyExists);
   }
 
-  createRepository(request: CreateRepositoryRequest): Promise<CreateRepositoryResponse> {
+  async delete(request: DeleteRequest): Promise<DeleteResponse> {
+    if (repositoryRequestValidator(request)) {
+      throw new Error(messages.repositoryNotProvided);
+    }
+
+    const repository = this.getRepository(request.repository);
+    if (request.key && !repository.hasOwnProperty(request.key)) {
+        throw new Error(`Configuration repository key ${request.key} not found`);
+    }
+
+    if (request.key) {
+      const { [request.key]: value, ...rest } = repository;
+      this.setRepository(request.repository, rest);
+    } else {
+      localStorage.removeItem(`${this.token}.${request.repository}`);
+    }
+    return {};
+  }
+
+  async entries(request: EntriesRequest): Promise<EntriesResponse<T>> {
     if (repositoryRequestValidator(request)) {
       return Promise.reject(new Error(messages.repositoryNotProvided));
     }
 
-    return new Promise((resolve, reject) => {
-      this.getRepository(request.repository).then(() => {
-        reject(new Error(messages.repositoryAlreadyExists));
-      }).catch(() => {
-        localStorage.setItem(`${this.token}.${request.repository}`, JSON.stringify({}));
-        return resolve({ repository: request.repository });
-      });
-    });
-  }
-
-  delete(request: DeleteRequest): Promise<DeleteResponse> {
-    if (repositoryRequestValidator(request)) {
-      return Promise.reject(new Error(messages.repositoryNotProvided));
-    }
-
-    return new Promise((resolve, reject) => {
-      this.getRepository(request.repository).then((repository) => {
-        if (request.key) {
-          if (!repository.hasOwnProperty(request.key)) {
-            reject(new Error(`Configuration repository key ${request.key} not found`));
-          }
-
-          const { [request.key]: value, ...rest } = repository;
-          localStorage.setItem(`${this.token}.${request.repository}`, JSON.stringify(rest));
-        } else {
-          localStorage.removeItem(`${this.token}.${request.repository}`);
-        }
-
-        resolve({});
-      }).catch(reject);
-    });
-  }
-
-  entries(request: EntriesRequest): Promise<EntriesResponse<T>> {
-    if (repositoryRequestValidator(request)) {
-      return Promise.reject(new Error(messages.repositoryNotProvided));
-    }
-
-    return this.getRepository(request.repository).then(repository => ({
-      entries: Object.keys(repository).map(key => ({ key, value: repository[key] }))
-    }));
+    const repository = this.getRepository(request.repository);
+    return { entries: Object.keys(repository).map(key => ({ key, value: repository[key] }))};
   };
 
-  fetch(request: FetchRequest): Promise<FetchResponse> {
+  async fetch(request: FetchRequest): Promise<FetchResponse> {
     if (repositoryRequestValidator(request)) {
-      return Promise.reject(new Error(messages.repositoryNotProvided));
+      throw new Error(messages.repositoryNotProvided);
     }
 
     if (repositoryKeyRequestValidator(request)) {
-      return Promise.reject(new Error(messages.repositoryKeyNotProvided));
+      throw new Error(messages.repositoryKeyNotProvided);
     }
 
-    return new Promise((resolve, reject) => {
-      this.getRepository(request.repository).then(repository =>
-        Object.keys(repository).indexOf(request.key) >= 0
-          ? resolve({ key: request.key, value: repository[request.key] })
-          : reject(new Error(`Configuration repository key ${request.key} not found`))
-      ).catch(reject);
-    });
+    const repository = this.getRepository(request.repository)
+
+    if (Object.keys(repository).indexOf(request.key) >= 0) {
+      return { key: request.key, value: repository[request.key] };
+    }
+
+    throw new Error(`Configuration repository key ${request.key} not found`);
   }
 
-  save(request: SaveRequest): Promise<SaveResponse> {
+  async save(request: SaveRequest): Promise<SaveResponse> {
     if (repositoryRequestValidator(request)) {
-      return Promise.reject(new Error(messages.repositoryNotProvided));
+      throw new Error(messages.repositoryNotProvided);
     }
 
     if (repositoryKeyRequestValidator(request)) {
-      return Promise.reject(new Error(messages.repositoryKeyNotProvided));
+      throw new Error(messages.repositoryKeyNotProvided);
     }
 
-    return new Promise((resolve, reject) => {
-      this.getRepository(request.repository).then((repository) => {
-        this.updateRepositoryData(request, repository);
-        resolve({});
-      }).catch(reject);
+    const repositoryData = this.getRepository(request.repository);
+    this.setRepository(request.repository, {
+      ...this.getRepository(request.repository),
+      [request.key]: request.value
     });
+    return {};
   }
 
   public createEntry(request: SaveRequest<T>) {
@@ -128,30 +119,22 @@ export class ConfigurationServiceLocalStorage<T=any> implements ConfigurationSer
     return this.changeEntry(request, 'update');
   };
 
-  private changeEntry(request: SaveRequest<T>, mode: 'create' | 'update') {
-    try {
-      validateSaveEntryRequest(request);
-    } catch (error) {
-      return Promise.reject(error);
+  private async changeEntry(request: SaveRequest<T>, mode: 'create' | 'update') {
+    validateSaveEntryRequest(request);
+    const repository = this.getRepository(request.repository);
+
+    if (mode === 'create' && repository.hasOwnProperty(request.key)) {
+      throw new Error(messages.entryAlreadyExist(request.key));
     }
 
-    return this.getRepository(request.repository)
-      .then(repositoryData => {
-        if (mode === 'create' && repositoryData.hasOwnProperty(request.key)) {
-          throw new Error(messages.entryAlreadyExist(request.key));
-        }
-        if (mode === 'update' && !repositoryData.hasOwnProperty(request.key)) {
-          throw new Error(messages.entryDoesNotExist(request.key));
-        }
-        return this.updateRepositoryData(request, repositoryData);
-      })
-  };
+    if (mode === 'update' && !repository.hasOwnProperty(request.key)) {
+      throw new Error(messages.entryDoesNotExist(request.key));
+    }
 
-  private updateRepositoryData = (request: SaveRequest<T>, repositoryData: object) => {
-    localStorage.setItem(`${this.token}.${request.repository}`, JSON.stringify({
-      ...repositoryData,
+    this.setRepository(request.repository, {
+      ...repository,
       [request.key]: request.value
-    }));
+    });
     return {};
-  }
+  };
 }
